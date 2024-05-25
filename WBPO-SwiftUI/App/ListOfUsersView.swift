6976d8a51f707701d653dab9add360ae393a034a
Tim-engineer <matlakt@outlook.com>
2024-05-25 15:04:28 +0200

    Changed navTitle and added an emoji

--- WBPO-SwiftUI/App/ListOfUsersView.swift
+++ WBPO-SwiftUI/App/ListOfUsersView.swift
@@ -20,7 +20,7 @@ struct ListOfUsersView: View {
                 }
             }
             .listStyle(.grouped)
-            .navigationTitle("Users")
+            .navigationTitle("List of users 👨🏼‍🦳")
             .navigationDestination(for: UserObject.self) { user in
                 UserDetail(user: user)
             }
